da4777e3ffa264d13d7a59a0a60d5414a12d770d
djmartyhouse <81476813+djmartyhouse@users.noreply.github.com>
2021-12-20 00:29:11 +0100

    Update and rename spamme.py to spBOT.py

--- userbot/plugins/spBOT.py
+++ userbot/plugins/spBOT.py
@@ -19,24 +19,15 @@ from . import BOTLOG, BOTLOG_CHATID
 
 id_chat=[]
 @catub.cat_cmd(
-    pattern=".savespam",
-    command=("savespam", "tools"),
+    pattern=".spBOT",
+    command=("spBOT", "tools"),
     info={
         "header": "Manda sticker",
         "description": "Cambia sticker",
         "usage": "{tr}ms",
     },
 )
-async def cambia(event, controllo=1):
-        chat1 = event.chat_id
-        
-        await edit_delete(event, f".savespam")
-        for x in id_chat:
-            if x == chat1:
-                controllo = 0
-                break
-        if controllo:
-            indice = len(id_chat)
-            id_chat.insert(indice, chat1)
-        await event.client.send_message(chat1,f"id_chat")
+async def cambia(event):
+    await event.client.send_message(event.chat_id,"Spam sticker avviato con successo!! ")
+
 
