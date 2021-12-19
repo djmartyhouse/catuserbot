f7065daa59017124e08837b7b7f128862b44f324
djmartyhouse <81476813+djmartyhouse@users.noreply.github.com>
2021-12-19 23:22:16 +0100

    Update spammer.py

--- userbot/plugins/spammer.py
+++ userbot/plugins/spammer.py
@@ -1,61 +1,39 @@
-from telethon.sync import TelegramClient, events
+import asyncio
+import re
+from telethon.errors import FloodWaitError
+from telethon import events
+from telethon.tl import functions, types
+from ..helpers import get_user_from_event, reply_id
+from . import catub, edit_or_reply, mention
+from . import (
+    _catutils,
+    catub,
+    edit_delete,
+    logging,
+)
 from userbot import catub
-plugin_category = "utils"
-id_chat = []
-    @catub.cat_cmd(
-        pattern=".save",
-        command=("gpic", plugin_category),
-        info={
-            "header": "For changing group display pic or deleting display pic",
-            "description": "Reply to Image for changing display picture",
-            "flags": {
-                "-s": "To set group pic",
-                "-d": "To delete group pic",
-            },
-            "usage": [
-                "{tr}gpic -s <reply to image>",
-                "{tr}gpic -d",
-            ],
-        },
-        groups_only=True,
-        require_admin=True,
-    )
-    async def handler(event, controllo=1):
-        chat1 = event.chat_id
-        await event.delete()
-        for x in id_chat:
-            if x == chat1:
-                controllo = 0
-                break
-        if controllo:
-            indice = len(id_chat)
-            id_chat.insert(indice, chat1)
-        print(id_chat)
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import InputStickerSetID
+from . import BOTLOG, BOTLOG_CHATID
 
 
-    @catub.cat_cmd(
-        pattern=".spam",
-        command=("gpic", plugin_category),
-        info={
-            "header": "For changing group display pic or deleting display pic",
-            "description": "Reply to Image for changing display picture",
-            "flags": {
-                "-s": "To set group pic",
-                "-d": "To delete group pic",
-            },
-            "usage": [
-                "{tr}gpic -s <reply to image>",
-                "{tr}gpic -d",
-            ],
-        },
-        groups_only=True,
-        require_admin=True,
-    )
-    async def handler(event):
-        await event.delete()
-        for x in id_chat:
-            await client.send_message(x, 'si no fesso')
-
-
-    client.run_until_disconnected()
-# Remember to use your own values from my.telegram.org!
+@catub.cat_cmd(
+    pattern=".save",
+    command=("ss", "tools"),
+    info={
+        "header": "Manda sticker",
+        "description": "Cambia sticker",
+        "usage": "{tr}ms",
+    },
+)
+async def cambia(event, controllo=1):
+    chat1 = event.chat_id
+            await event.client.delete()
+            for x in id_chat:
+                if x == chat1:
+                    controllo = 0
+                    break
+            if controllo:
+                indice = len(id_chat)
+                id_chat.insert(indice, chat1)
+            print(id_chat)
